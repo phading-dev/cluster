1fc76c896fc253d077a0e14a810e77a7a06af64b
Ykuyo <ykuyo.dev@gmail.com>
2025-08-03 21:00:45 -0700

    Added extra domains.

--- env_vars.ts
+++ env_vars.ts
@@ -22,6 +22,7 @@ export interface ClusterEnvVars {
   initCreditAmount?: number;
   externalDomain?: string;
   externalOrigin?: string;
+  externalSynonymDomains?: Array<string>;
   internalOrigin?: string;
   platformName?: string;
   supportEmailName?: string;
